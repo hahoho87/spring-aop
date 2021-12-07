9ce4c800a4bf67a0772f922dd90f66aec5313135
bky4564 <bky4564@hunet.co.kr>
2021-12-07 12:38:55 +0900

    Spring AOP pointcut - bean 필터 적용

---
+++ src/test/java/com/hahoho87/springaop/pointcut/AtBeanTest.java
@@ -0,0 +1,34 @@
+package com.hahoho87.springaop.pointcut;
+
+import com.hahoho87.springaop.order.OrderService;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Slf4j
+@Import(AtBeanTest.BeanAspect.class)
+@SpringBootTest
+public class AtBeanTest {
+
+    @Autowired
+    OrderService orderService;
+
+    @Test
+    void successTest() {
+        orderService.orderItem("itemA");
+    }
+
+    @Aspect
+    static class BeanAspect {
+        @Around("bean(orderService) || bean(*repository)")
+        public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable {
+            log.info("[bean] {}", joinPoint.getSignature());
+            return joinPoint.proceed();
+        }
+    }
+}
