51aa6d40573d054e1feaac56e789e372bb157a1a
hahoho87 <bbaing4564@gmail.com>
2021-12-07 23:00:20 +0900

    예제 - 로그 출력 aop 생성

---
+++ src/main/java/com/hahoho87/springaop/exam/ExamRepository.java
@@ -0,0 +1,23 @@
+package com.hahoho87.springaop.exam;
+
+import com.hahoho87.springaop.exam.annotation.Trace;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ExamRepository {
+
+    private static int seq = 0;
+
+    /**
+     * 5번에 한 번 실패하는 요청
+     */
+    @Trace
+    public String save(String itemId) {
+        seq++;
+        if (seq % 5 == 0) {
+            throw new IllegalStateException("예외 발생");
+        }
+        return "ok";
+    }
+
+}
---
+++ src/main/java/com/hahoho87/springaop/exam/ExamService.java
@@ -0,0 +1,17 @@
+package com.hahoho87.springaop.exam;
+
+import com.hahoho87.springaop.exam.annotation.Trace;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ExamService {
+
+    private final ExamRepository examRepository;
+
+    @Trace
+    public void request(String itemId) {
+        examRepository.save(itemId);
+    }
+}
